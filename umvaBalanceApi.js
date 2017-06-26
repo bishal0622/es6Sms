66ccdcf978c3486ee6d349b76f56ba2fc6a4035f
bishal <monster_bishal@live.com>
2017-06-26 21:35:43 +0545

    Sa

--- umvaBalanceApi.js
+++ umvaBalanceApi.js
@@ -1,3 +1,5 @@
+var umvaResponse;
+
 module.exports = function(from){
 
     var Client = require('node-rest-client').Client;
@@ -8,14 +10,13 @@ module.exports = function(from){
 
     var phoneNumberOnly = from.substring(1);
 
-    var umvaResponse;
-
     client.get("http://business.capaz.org/sms_balanceEnquiry?&mobile=%2B" + phoneNumberOnly + "&token=" + umvaToken, function (data, response) {
         console.log(JSON.parse(data));
         umvaResponse = JSON.parse(data);
-        console.log("umva res:::::",umvaResponse);
 
     });
+
+    console.log("umva res:::::",umvaResponse);
 
     return umvaResponse;
 }
